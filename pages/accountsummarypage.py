fa7c4dcff86bf5373d7b0108f743ddbdecaa54d2
Naveen.S <naveensagayaselvaraj@gmail.com>
2020-05-17 12:37:31 +0530

    update accountsummarypage.py

--- pages/accountsummarypage.py
+++ pages/accountsummarypage.py
@@ -1,12 +1,13 @@
 from pages.basepage import BasePage
 from selenium.webdriver.common.by import By
-
+import time
 
 class AccountSummaryPage(BasePage):
     pay_bills_link = By.XPATH, "//a[text()='Pay Bills']"
     title_link = By.XPATH, "//a[text()='Zero Bank']"
 
     def click_pay_bills_link(self):
+        time.sleep(5)
         self._click(self.pay_bills_link)
 
     def click_title_link(self):
